3f598ea77aa509799ae86a9d801024596d8d2b87
JerinPaulS <jerinsprograms@gmail.com>
2024-05-11 19:54:59 +0530

    Files

---
+++ src/main/java/generics/wildcards/Employee.java
@@ -0,0 +1,21 @@
+package org.example.generics.wildcards;
+
+import java.text.MessageFormat;
+
+public class Employee {
+
+    private String name;
+
+    public Employee(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return MessageFormat.format("Employee'{'name=''{0}'''}'", name);
+    }
+}
