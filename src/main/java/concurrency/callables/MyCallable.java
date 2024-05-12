2a779d27672a6de73a269b77dc675fc1a8a5b04d
JerinPaulS <jerinsprograms@gmail.com>
2024-05-12 09:11:59 +0530

    Callables

---
+++ src/main/java/concurrency/callables/MyCallable.java
@@ -0,0 +1,24 @@
+package org.example.concurrency.callables;
+
+import java.text.MessageFormat;
+import java.util.concurrent.Callable;
+
+public class MyCallable implements Callable<String> {
+
+    private int id;
+
+    public MyCallable(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String call() throws Exception {
+        return String.format("%s using thread %s",
+                this, Thread.currentThread().getName());
+    }
+
+    @Override
+    public String toString() {
+        return MessageFormat.format("MyCallable'{'id={0}'}'", id);
+    }
+}
