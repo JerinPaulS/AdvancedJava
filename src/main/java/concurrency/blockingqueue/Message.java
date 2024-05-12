c3f0f3bbab49c093813000607b4b38d264042468
JerinPaulS <jerinsprograms@gmail.com>
2024-05-12 10:23:56 +0530

    Producer consume

---
+++ src/main/java/concurrency/blockingqueue/Message.java
@@ -0,0 +1,15 @@
+package org.example.concurrency.blockingqueue;
+
+public class Message {
+
+    private final int id;
+
+    public Message(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+}
