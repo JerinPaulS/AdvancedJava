d1338216c1feba25db4c107477164732baee3412
JerinPaulS <jerinsprograms@gmail.com>
2024-05-12 10:36:55 +0530

    JDBC

---
+++ src/main/java/database/jdbc/PersonDao.java
@@ -0,0 +1,13 @@
+package org.example.database.jdbc;
+
+import java.util.List;
+
+public interface PersonDao {
+
+    List<Person> findAll();
+    Person findById(Integer id);
+    Integer save(Person p);
+    void delete(Person p);
+    List<Integer> getIds();
+
+}
