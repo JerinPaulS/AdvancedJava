38c6be420ad63f29b6ca96d73ea06e243ce7a7cb
JerinPaulS <jerinsprograms@gmail.com>
2024-05-14 08:48:55 +0530

    Lazy streams

---
+++ src/main/java/lambdasstreams/Summarizing.java
@@ -0,0 +1,17 @@
+package lambdasstreams;
+
+import java.util.Collections;
+import java.util.DoubleSummaryStatistics;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Summarizing {
+
+    public static void main(String[] args) {
+        DoubleSummaryStatistics stats = Stream.generate(Math::random)
+                .limit(1000)
+                .collect(Collectors.summarizingDouble(Double::doubleValue));
+        System.out.println(stats);
+    }
+
+}
